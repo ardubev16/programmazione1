51f14578d2316ce660e810add386e067443efc8d
ardubev16 <43483037+ardubev16@users.noreply.github.com>
2021-10-21 22:33:56 +0200

    efra_fix, div_function.cc

--- efra_fix/div_function.cc
+++ efra_fix/div_function.cc
@@ -1,26 +1,46 @@
 using namespace std;
 #include <iostream>
 
-int divisione (int risultato, int *resto, int dividendo, int divisore) {
-    int dividendo2;
-    for (risultato = 0; dividendo >= divisore; risultato++) {
-        dividendo2 = dividendo - divisore;
-        dividendo = dividendo2;
+void divisione(int * risultato, int * resto, int dividendo, int divisore) {
+    for (*risultato = 0; dividendo >= divisore; (*risultato)++)
+        dividendo -= divisore;
+    *resto = dividendo;
+}
+
+int divisione_while(int dividendo, int divisore, int * resto) {
+    int risultato = 0;
+    while (dividendo >= divisore) {
+        dividendo -= divisore;
+        risultato++;
     }
-    if (dividendo < divisore) {
-        dividendo = *resto;
-    }
+    *resto = dividendo;
+
     return risultato;
 }
+
+int divisione_rec(int dividendo, int divisore, int & resto) {
+    int risultato;
+    if (dividendo < divisore) {
+        resto = dividendo;
+        risultato = 0;
+    } else
+        risultato = 1 + divisione_rec(dividendo - divisore, divisore, resto);
+
+    return risultato;
+}
+
 
 int main () {
     int dividendo, divisore, risultato, resto;
     cout << "Scrivi due numeri interi e ti dirò risultato della divisione tra loro, e il resto." << endl;
-    cin >> dividendo;
-    cin >> divisore;
+    cin >> dividendo >> divisore;
 
-    divisione (risultato, &resto, dividendo, divisore);
-    
+    divisione (&risultato, &resto, dividendo, divisore);
     cout << "Il risultato  è " << risultato << " e il resto è " << resto << endl;
+
+    cout << "a/b = " << divisione_while(dividendo, divisore, &resto) << ", R = " << resto << endl;
+    
+    cout << "rec a/b = " << divisione_rec(dividendo, divisore, resto) << ", R = " << resto << endl;
+
     return 0;
 }
